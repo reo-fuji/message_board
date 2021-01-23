240b0c4bf396ef41a8f8ae00f1a7ec161c121adb
reo <taiger1825@gmail.com>
2021-01-23 12:58:55 +0000

    add column

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -26,12 +26,26 @@
     {{-- メッセージ作成ページへのリンク --}}
     {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
 
-    @foreach ($messages as $message)
-    <tr>
-        {{-- メッセージ詳細ページへのリンク --}}
-        <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
-        <td>{{ $message->content }}</td>
-    </tr>
-    @endforeach
+    @if (count($messages) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($messages as $message)
+                <tr>
+                    {{-- メッセージ詳細ページへのリンク --}}
+                    <td>{!! link_to_route('messages.show', $message->id, ['message' => $message->id]) !!}</td>
+                    <td>{{ $message->title }}</td>
+                    <td>{{ $message->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
                 
 @endsection
