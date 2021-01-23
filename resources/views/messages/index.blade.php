c6e8a6606ed7a6ec87a20da1c23fdee0344bf25d
reo <taiger1825@gmail.com>
2021-01-23 14:25:31 +0000

    add seeder

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -9,28 +9,6 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>メッセージ</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($messages as $message)
-                <tr>
-                    <td>{{ $message->id }}</td>
-                    <td>{{ $message->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
-
-    @if (count($messages) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
                     <th>タイトル</th>
                     <th>メッセージ</th>
                 </tr>
@@ -47,5 +25,8 @@
             </tbody>
         </table>
     @endif
+
+    {{-- メッセージ作成ページへのリンク --}}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
                 
 @endsection
